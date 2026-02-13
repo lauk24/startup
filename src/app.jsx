429ed5f9da0432abc8d22b8943c291bb3b08fb5e
Kent Lau <kentlau@Kents-MacBook-Pro-5.local>
2026-02-12 21:08:37 -0700

    Finished app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body">
+        <header>
+            <h1>SoundEscape</h1>
+            <form action="search-results.html" method="get" className="search-form">
+                <input type="text" name="q" placeholder="Search for a song" className="search-input"/>
+                <button type="submit" className="btn btn-primary">Search</button>
+            </form>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="profile.html">Profile</a></li>
+                    <li><a href="library.html">My Library</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            App Components go here
+        </main>
+        <footer>
+            <hr />
+            <a href="https://github.com/lauk24/startup">GitHub</a>
+        </footer>
+  </div>
+  )
 }
