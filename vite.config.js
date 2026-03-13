d948800f9fbbc6ed028de5eba988a4edd9fc0a0b
Kent Lau <kent.lau1248@gmail.com>
2026-03-12 22:26:42 -0600

    Finished building app. Still need to make 3rd party calls

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    outDir: './service/public',
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
