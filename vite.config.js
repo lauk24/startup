e87c454b0e63428776cea25510e02e4f99b361d4
Kent Lau <kent.lau1248@gmail.com>
2026-03-12 23:56:14 -0600

    Fixed reviews not showing up on library

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    outDir: './service/public',
-  },
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
