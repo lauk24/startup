084a87db77f8f0835d989a30dfb0f5a0bbb69914
Kent Lau <kent.lau1248@gmail.com>
2026-03-24 21:19:28 -0600

    Added the connection for storing and authenticating users

--- service/database.js
+++ service/database.js
@@ -4,16 +4,40 @@ const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
 const client = new MongoClient(url);
-const db = client.db('rental');
-const collection = db.collection('house');
+const db = client.db('soundescape');
 
-async function main() {
+const userCollection = db.collection('user');
+const libraryCollection = db.collection('library');
+const globalRatingCollection = db.collection('globalRating');
+
+(async function testConnection() {
   try {
-    // add all the following database code here
-    
-  } finally {
-    client.close();
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
   }
+})();
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function updateUserRemoveAuth(user) {
+  await userCollection.updateOne({ email: user.email }, { $unset: { token: 1 } });
 }
 
 main();
