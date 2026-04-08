1dd631bced0d50ad4c02eebf64054a53319c0d5f
Kent Lau <kent.lau1248@gmail.com>
2026-04-07 20:54:46 -0600

    Added websocket to front and back end

---
+++ src/gameNotifier.js
@@ -0,0 +1,54 @@
+const MusicEvent = {
+  System: 'system',
+  NewRating: 'newRating',
+};
+
+class EventMessage {
+  constructor(from, type, value) {
+    this.from = from;
+    this.type = type;
+    this.value = value;
+  }
+}
+
+class MusicEventNotifier {
+  handlers = [];
+
+  constructor() {
+    let port = window.location.port;
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+    this.socket.onopen = (event) => {
+      this.receiveEvent(new EventMessage('system', MusicEvent.System, { msg: 'connected' }));
+    };
+    this.socket.onclose = (event) => {
+      this.receiveEvent(new EventMessage('system', MusicEvent.System, { msg: 'disconnected' }));
+    };
+    this.socket.onmessage = async (msg) => {
+      try {
+        const event = JSON.parse(await msg.data.text());
+        this.receiveEvent(event);
+      } catch {}
+    };
+  }
+
+  broadcastEvent(from, type, value) {
+    const event = new EventMessage(from, type, value);
+    this.socket.send(JSON.stringify(event));
+  }
+
+  addHandler(handler) {
+    this.handlers.push(handler);
+  }
+
+  removeHandler(handler) {
+    this.handlers = this.handlers.filter((h) => h !== handler);
+  }
+
+  receiveEvent(event) {
+    this.handlers.forEach((handler) => handler(event));
+  }
+}
+
+const MusicNotifier = new MusicEventNotifier();
+export { MusicEvent, MusicNotifier };
